dd6fe4be4e4ca5d1ae6bfa0a7ec9313237f603f7
roger-roger-svg <rogercunhadeandrade@gmail.com>
2023-10-30 10:57:24 -0300

    Permissao de localizacao para o usuario

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { styles } from "./style";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+} from "expo-location"; //permissao para acessar a localizacao do usuario
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+      console.log("localizacao atual =>", currentPosition);
+    }
+  }
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
   return <View style={styles.container}></View>;
 }
