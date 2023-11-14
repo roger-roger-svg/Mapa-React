f1df625ad7fe64d12ac80b8ae0203f0eb8b356cd
roger-roger-svg <rogercunhadeandrade@gmail.com>
2023-11-13 22:24:31 -0300

    Todas as maravilhas add

--- App.tsx
+++ App.tsx
@@ -22,10 +22,41 @@ export default function App() {
       flagIcon: "🇧🇷",
     },
     {
-      name: "Great Wall of China",
+      name: "Grande Muralha da China",
       latitude: 40.4319,
       longitude: 116.5704,
       flagIcon: "🇨🇳",
+    },
+    {
+      name: "Machu Picchu",
+      latitude: -13.2263,
+      longitude: -72.4973,
+      flagIcon: "🇵🇪",
+    },
+    {
+      name: "Chichén Itzá",
+      flagIcon: "🇲🇽",
+      latitude: 20.6843,
+      longitude: -88.5678,
+    },
+
+    {
+      name: "Coliseu",
+      flagIcon: "🇮🇹",
+      latitude: 41.8902,
+      longitude: 12.4922,
+    },
+    {
+      name: "Taj Mahal",
+      flagIcon: "🇮🇳",
+      latitude: 27.1751,
+      longitude: 78.0421,
+    },
+    {
+      name: "Ruínas de Petra",
+      flagIcon: "🇯🇴",
+      latitude: 30.3285,
+      longitude: 35.4428,
     },
   ]);
 
